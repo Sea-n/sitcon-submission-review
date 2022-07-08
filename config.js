6a9065cf4829576eae57b714b9646f8eae4020b0
Denny Huang <denny0223@gmail.com>
2022-07-08 17:42:58 +0800

    Update for 開放式議程

--- config.js
+++ config.js
@@ -3,14 +3,14 @@ window.CONFIG = {
   fields: [
     { name: '您的姓名 / 暱稱 Name', label: '您的姓名 / 暱稱 Name'},
     { name: '題目 Title', label: '題目 Title'},
-    { name: '投稿類型 Type', label: '投稿類型 Type'},
     { name: '摘要 Abstract', label: '摘要 Abstract'},
     { name: '自我介紹 Self Introduction', label: '自我介紹 Self Introduction'},
-    { name: '演講大綱 Outline', label: '演講大綱 Outline'},
+    { name: '進行模式 Method ', label: '進行模式 Method '},
+    { name: '議程大綱 Outline Outline', label: '議程大綱 Outline Outline'},
     { name: '目標受眾 Target Audience', label: '目標受眾 Target Audience'},
     { name: '先備知識 Prior Knowledge', label: '先備知識 Prior Knowledge'},
     { name: '詳細說明 Description', label: '詳細說明 Description'},
-    { name: '附件 Attachments', label: '附件 Attachments'},
+    { name: '附件 Attachment', label: '附件 Attachment'},
   ],
   skipRows: 3
 }
