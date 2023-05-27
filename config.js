ec6bd1a9847e1a350b9bd22d0e5e935d78121931
Denny Huang <denny0223@gmail.com>
2023-05-27 10:54:53 +0800

    Update for 2023 Student Adventure Time

--- config.js
+++ config.js
@@ -2,15 +2,14 @@ window.CONFIG = {
   // dataFileName: 'demo-submission.html',
   fields: [
     { name: '您的姓名 / 暱稱 Name', label: '您的姓名 / 暱稱 Name'},
-    { name: '題目 Title', label: '題目 Title'},
-    { name: '摘要 Abstract', label: '摘要 Abstract'},
+    { name: '題目 Title'                 , label: '題目 Title'},
+    { name: '投稿類型 Type', label: '投稿類型 Type'},
+    { name: '摘要 Abstract'              , label: '摘要 Abstract'},
     { name: '自我介紹 Self Introduction', label: '自我介紹 Self Introduction'},
-    { name: '進行模式 Method ', label: '進行模式 Method '},
-    { name: '議程大綱 Outline Outline', label: '議程大綱 Outline Outline'},
     { name: '目標受眾 Target Audience', label: '目標受眾 Target Audience'},
-    { name: '先備知識 Prior Knowledge', label: '先備知識 Prior Knowledge'},
+    { name: '內容難易度 Difficulty', label: '內容難易度 Difficulty'},
     { name: '詳細說明 Description', label: '詳細說明 Description'},
-    { name: '附件 Attachment', label: '附件 Attachment'},
+    { name: '其他投稿相關補充資料 Supplemental materials', label: '其他投稿相關補充資料 Supplemental materials'},
   ],
   skipRows: 3
 }
